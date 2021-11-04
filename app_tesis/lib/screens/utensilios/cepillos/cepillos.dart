89bede88600637f0d01f997081df6383a8079a5f
Tomasespinozagarrido <tomasmate.teg@gmail.com>
2021-11-04 13:34:17 -0300

    Create cepillos.dart

---
+++ app_tesis/lib/screens/utensilios/cepillos/cepillos.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class cepillos extends StatefulWidget {
+  const cepillos({Key? key}) : super(key: key);
+
+  @override
+  _cepillosState createState() => _cepillosState();
+}
+
+class _cepillosState extends State<cepillos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
