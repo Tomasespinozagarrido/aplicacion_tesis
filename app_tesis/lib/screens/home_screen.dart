2b64a74caa400d1211a5d5037da38d08e225ebf3
Tomasespinozagarrido <tomasmate.teg@gmail.com>
2021-11-19 13:08:28 -0300

    barra inferior solo con boton inicio

--- app_tesis/lib/screens/home_screen.dart
+++ app_tesis/lib/screens/home_screen.dart
@@ -19,64 +19,40 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(),
       drawer: Drawer(
         child: ListView(
-        children:[
-          DrawerHeader(
-            decoration: BoxDecoration(
-            gradient: LinearGradient(colors: <Color>[
-              Colors.lightBlue,
-              Colors.blue
-            ])
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: <Color>[Colors.lightBlue, Colors.blue])),
+              child: Text('Drawer header '),
             ),
-            child: Text('Drawer header '),
-          ),
-          CustomListTile(Icons.person,'Perfil'),
-          CustomListTile(Icons.notifications,'Notificaciones'),
-          CustomListTile(Icons.settings,'Opciones'),
-          CustomListTile(Icons.lock,'cerrar sesión'),
+            CustomListTile(Icons.person, 'Perfil'),
+            CustomListTile(Icons.notifications, 'Notificaciones'),
+            CustomListTile(Icons.settings, 'Opciones'),
+            CustomListTile(Icons.lock, 'cerrar sesión'),
           ],
         ),
       ),
-      body: PageView(
-        controller: pageController,
-        physics: NeverScrollableScrollPhysics(),
-        children: [
-          Inicio(),
-          Utensilios(),
-          Riesgos(),
-          Cuidados(),
-        ],
+      body: Inicio(),
+      bottomNavigationBar: Container(
+        height: 60,
+        color: Colors.black12,
+        child: InkWell(
+          onTap: () => print('Volver al inicio'),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.home,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                const Text('Inicio'),
+              ],
+            ),
+          ),
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: currentPage,
-          onTap: (index) {
-            currentPage = index;
-            var animateToPage = pageController.animateToPage(index,
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeOut);
-            setState(() {});
-          },
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Inicio',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Utensilios',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.dangerous),
-              label: 'Riesgos',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.health_and_safety_sharp),
-              label: 'Cuidados',
-            ),
-          ],
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.blue,
-          selectedItemColor: Colors.white,
-          unselectedItemColor: Colors.cyan.shade50),
     );
   }
 }
@@ -97,41 +73,39 @@ class CustomScreen extends StatelessWidget {
   }
 }
 
-class CustomListTile extends StatelessWidget{
+class CustomListTile extends StatelessWidget {
 //constructor del custom list tile
   IconData icon;
   String text;
 
-  CustomListTile(this.icon,this.text);
-//termino del constructor  
+  CustomListTile(this.icon, this.text);
+//termino del constructor
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
       child: InkWell(
         splashColor: Colors.cyan,
-        onTap: ()=>{},
+        onTap: () => {},
         child: Container(
           height: 40,
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children:<Widget>[
-              Row(
-                children:<Widget>[
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(children: <Widget>[
                   Icon(icon),
-                  Padding(padding: const EdgeInsets.all(8.0),
-                  child: Text(text,style: TextStyle(
-                    fontSize: 16.0,
-                    )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(text,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        )),
                   )
-                ]
-              ),
-            Icon(Icons.arrow_right)
-            ]
-          ),
+                ]),
+                Icon(Icons.arrow_right)
+              ]),
         ),
       ),
     );
-
   }
 }
