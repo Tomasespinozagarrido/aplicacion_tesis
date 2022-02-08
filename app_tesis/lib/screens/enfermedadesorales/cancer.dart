58f998860c0050974316246d727ae95ed0e53f8b
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2022-02-08 20:17:02 -0300

    chewie sin funcionar

--- app_tesis/lib/screens/enfermedadesorales/cancer.dart
+++ app_tesis/lib/screens/enfermedadesorales/cancer.dart
@@ -3,19 +3,94 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-
+import 'package:chewie/chewie.dart';
+import 'package:video_player/video_player.dart';
 class Cancer extends StatefulWidget {
-  const Cancer({ Key? key }) : super(key: key);
+  final VideoPlayerController videoPlayerController;
+  final bool looping;
+
+  Cancer({
+    @required this.videoPlayerController,
+    this.looping,
+    Key key;
+  }):super(key: key)
 
   @override
   _CancerState createState() => _CancerState();
 }
 
 class _CancerState extends State<Cancer> {
+ ChewieController _chewieController;
+  @override
+  void initState() {
+    super.initState();
+    // Wrapper on top of the videoPlayerController
+    _chewieController = ChewieController(
+      videoPlayerController: widget.videoPlayerController,
+      aspectRatio: 16 / 9,
+      // Prepare the video to be played and display the first frame
+      autoInitialize: true,
+      looping: widget.looping,
+      // Errors can occur for example when trying to play a video
+      // from a non-existent URL
+      errorBuilder: (context, errorMessage) {
+        return Center(
+          child: Text(
+            errorMessage,
+            style: TextStyle(color: Colors.white),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Chewie(
+        controller: _chewieController,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // IMPORTANT to dispose of all the used resources
+    widget.videoPlayerController.dispose();
+    _chewieController.dispose();
+  }
+}
+
+class CancerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Video Player'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Cancer(
+            videoPlayerController: VideoPlayerController.asset(
+              'videos/IntroVideo.mp4',
+            ),
+            looping: true,
+          ),
+          Cancer(
+            videoPlayerController: VideoPlayerController.network(
+              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+            ),
+          ),
+          Cancer(
+            // This URL doesn't exist - will display an error
+            videoPlayerController: VideoPlayerController.network(
+              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/error.mp4',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
