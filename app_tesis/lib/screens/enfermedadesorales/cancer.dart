37bc93d8ace2b1cd3268a8373e1169519323d3e1
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2022-02-08 16:27:33 -0300

    cambios

--- app_tesis/lib/screens/enfermedadesorales/cancer.dart
+++ app_tesis/lib/screens/enfermedadesorales/cancer.dart
@@ -1,52 +1,21 @@
-import 'dart:html';
-import 'videostest.dart';
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 import 'package:app_tesis/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-import 'package:chewie/chewie.dart';
-import 'package:video_player/video_player.dart';
 
 class Cancer extends StatefulWidget {
-
-
-  const Cancer({ Key ? key }) : super(key: key);
+  const Cancer({ Key? key }) : super(key: key);
 
   @override
   _CancerState createState() => _CancerState();
 }
 
 class _CancerState extends State<Cancer> {
- @override
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Video Player'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.asset(
-              'videos/IntroVideo.mp4',
-            ),
-            looping: true,
-          ),
-          ChewieListItem(
-            videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-            ),
-          ),
-          ChewieListItem(
-            // This URL doesn't exist - will display an error
-            videoPlayerController: VideoPlayerController.network(
-              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/error.mp4',
-            ),
-          ),
-        ],
-      ),
+    return Container(
+      
     );
   }
 }
