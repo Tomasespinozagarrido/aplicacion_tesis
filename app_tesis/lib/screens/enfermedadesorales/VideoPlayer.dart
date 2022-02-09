a0df8b6cce0cfbaa3b5ff5312854ae8e581e48cb
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2022-02-09 20:50:17 -0300

    cambios para poder hacer el reproductor de video

---
+++ app_tesis/lib/screens/enfermedadesorales/VideoPlayer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayer extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  const VideoPlayer( Key key, @required this.controller, ) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
