8e5388cea6b9ff8954b1d558849dce99f1d80627
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2022-02-10 18:20:18 -0300

    permisos

--- app_tesis/lib/screens/enfermedadesorales/cancer.dart
+++ app_tesis/lib/screens/enfermedadesorales/cancer.dart
@@ -1,39 +1,70 @@
-import 'dart:ffi';
-
 import 'package:app_tesis/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-import 'package:video_player/video_player.dart';
 
 class Cancer extends StatefulWidget {
+  const Cancer({ Key? key }) : super(key: key);
 
   @override
   _CancerState createState() => _CancerState();
 }
 
 class _CancerState extends State<Cancer> {
-  final assets = 'videos/AT-cm_1102546002.mp4';
-  VideoPlayerController controller;
+  late VideoPlayerController _controller ;
+  late Future<void> _initializeVideoPlayerFuture;
 
   @override
-  void initState(){
+  void iniState(){
     super.initState();
-    controller = VideoPlayerController.asset(assets)
-    ..addListener(() => setState(() {}))
-    ..setLooping(true)
-    ..initialize().then((_) => controller.play());
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+    _initializeVideoPlayerFuture = _controller.initialize();
+    _controller.setLooping(true);
   }
-
-  @override 
+  
+  @override
   void dispose(){
-    controller.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
-  @override 
-  Widget build(BuildContext context){
-    return VideoPlayer(controller: controller);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('prueba de video'),
+      ),
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            );
+          }else{
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState((){
+              if(_controller.value.isPlaying){
+                _controller.pause();
+              }else{
+                _controller.play();
+              }
+            });
+        },
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
   }
 }
