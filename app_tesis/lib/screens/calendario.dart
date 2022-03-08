6854c74a933bac04d2abbc78fda83acd067befe2
Tomas Espinoza <tomasmate.teg@gmail.com>
2022-03-07 21:29:08 -0300

    calendario en español

--- app_tesis/lib/screens/calendario.dart
+++ app_tesis/lib/screens/calendario.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class CalendarWidget extends StatelessWidget {
   const CalendarWidget({Key? key}) : super(key: key);
 
-  @override
   Widget build(BuildContext context) {
-    return SfCalendar(
-      view: CalendarView.month,
-      backgroundColor: Colors.white,
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('es'),
+      ],
+      locale: const Locale('es'),
+      title: 'Calendar Localization',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Calendar'),
+        ),
+        body: SfCalendar(
+          view: CalendarView.month,
+        ),
+      ),
     );
   }
 }
