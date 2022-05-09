2600e41b75b92acdc1962fd755bd5c8fc1dbf069
Gonzalo Jaques <gonza.a.jaques17@gmail.com>
2022-05-09 14:55:23 -0400

    nueva distribucion

---
+++ app_tesis/lib/screens/screens.dart
@@ -0,0 +1,24 @@
+//menus iniciales
+export 'package:app_tesis/screens/riesgos.dart';
+export 'package:app_tesis/screens/cuidados.dart';
+export 'package:app_tesis/screens/inicio.dart';
+//utensilios
+export 'package:app_tesis/screens/utensilios.dart';
+export 'package:app_tesis/screens/utensilios/cepillos/cepillos.dart';
+//enfermedades
+export 'package:app_tesis/screens/enfermedadesorales/cancer.dart';
+export 'package:app_tesis/screens/enfermedadesorales/candidiasis.dart';
+export 'package:app_tesis/screens/enfermedadesorales/caries.dart';
+export 'package:app_tesis/screens/enfermedadesorales/enfermedadesperiodontales.dart';
+export 'package:app_tesis/screens/enfermedadesorales/menuenfermedadesorales.dart';
+export 'package:app_tesis/screens/enfermedadesorales/xerostomia.dart';
+//higiene
+export 'package:app_tesis/screens/cuidadosorales/dieta.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/MenuHigiene.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/cepillado/CepillosDentales.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/cepillado/CepillosInterprox.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/cepillado/HigieneDiaria.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/cepillado/Menucepillado.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/cepillado/TecnicaHigiene.dart';
+export 'package:app_tesis/screens/cuidadosorales/higiene/protesis/MenuProtesis.dart';
+export 'package:app_tesis/screens/cuidadosorales/menucuidadosorales.dart';
